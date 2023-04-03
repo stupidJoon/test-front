94e94924e3889a2a9634aac213b15bba2a3d07c3
JunSangYu <35032401+stupidJoon@users.noreply.github.com>
2023-04-03 14:46:15 +0000

    04.03

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+function Index() {
+  return (
+    <>
+      <header>
+        <hgroup>
+          <h1>Class-less example</h1>
+          <h2>Just a pure semantic HTML markup, without <code>.classes</code></h2>
+        </hgroup>
+      </header>
+      <main>
+        <section>
+          <form>
+            <input />
+            <button>검색</button>
+          </form>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default Index;
